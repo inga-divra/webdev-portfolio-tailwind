8fa51d904f3153976f4b914e7d6ae87199e88ed4
inga-divra <ingady@gmail.com>
2024-07-30 20:43:33 +0300

    Set up a responsive Navbar component with imported link data, applying Tailwind CSS classes for styling and hover effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-
+import Navbar from './components/Navbar'
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <>
+    <Navbar />
+  </>
 }
 
 export default App
