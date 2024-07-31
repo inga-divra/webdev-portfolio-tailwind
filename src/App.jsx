18e7690f04699206508e6c72cf72fa9c35157b2a
inga-divra <ingady@gmail.com>
2024-07-31 19:27:48 +0300

    Created Skills section with SkillsCard and SectionTitle components, and rendered it in App.jsx, using a skills array from data.jsx
    - Skills.jsx: Imported and used SkillsCard and SectionTitle components to render the skills section.
    
    - SectionTitle.jsx: Created a component to display section titles.
    
    - SkillsCard.jsx: Created a component to display individual skill cards.
    
    - index.html: Applied global styles for background color and smooth scrolling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-
+import Skills from './components/Skills'
 const App = () => {
   return <>
     <Navbar />
     <Hero />
+    <Skills />
   </>
 }
 
